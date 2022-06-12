24b9e008689f04fea84cc6c4572794c7034010f2
Paul Jongmans <pauul.win@gmail.com>
2022-06-12 19:03:47 +0200

    example route

--- routes/calender_routes.js
+++ routes/calender_routes.js
@@ -9,26 +9,14 @@ router.use((req, res, next) => {
     next();
 });
 
-router.post('/buildings', body('token').isString(), (req, res) => {
+router.post('/test', body('token').isString(), (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
-    tokenUtils.isTokenValid(req.body['token']).then((result) => {
-        if (!result.valid) return res.status(400).json({ error: 'Invalid token' });
-
-        database.sql.connect(database.sqlConfig).then((pool) => {
-            pool.query('SELECT * FROM [Terminator].[dbo].[buildings]')
-                .then((result) => {
-                    return res.status(200).json(result.recordset);
-                })
-                .catch((selectError) => {
-                    return res.status(400).json({ error: 'data_error_reading_logs' });
-                });
-        });
-    });
+    res.status(200).json([]);
 });
 
 module.exports = router;
