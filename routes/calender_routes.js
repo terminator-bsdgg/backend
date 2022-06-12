707df585e4c8c79f67cb164d5f990f38fc09215e
Paul Jongmans <pauul.win@gmail.com>
2022-06-12 16:12:25 +0200

    added buildings

--- routes/calender_routes.js
+++ routes/calender_routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const os = require('os');
 const { body, validationResult } = require('express-validator');
+const database = require('./../database');
+const tokenUtils = require('./../lib/tokenUtils');
 
 const router = express.Router();
 
@@ -8,15 +9,26 @@ router.use((req, res, next) => {
     next();
 });
 
-router.post('/data', body('draw').optional().isNumeric(), (req, res) => {
+router.post('/buildings', body('token').isString(), (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
-    console.log('Test');
-    return res.status(200).json([]);
+    tokenUtils.isTokenValid(req.body['token']).then((result) => {
+        if (!result.valid) return res.status(400).json({ error: 'Invalid token' });
+
+        database.sql.connect(database.sqlConfig).then((pool) => {
+            pool.query('SELECT * FROM [Terminator].[dbo].[buildings]')
+                .then((result) => {
+                    return res.status(200).json(result.recordset);
+                })
+                .catch((selectError) => {
+                    return res.status(400).json({ error: 'data_error_reading_logs' });
+                });
+        });
+    });
 });
 
 module.exports = router;
