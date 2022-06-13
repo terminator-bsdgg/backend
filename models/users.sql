8366a1bc0dc38523290a23a1af754c9b2cc4ea58
Paul Jongmans <pauul.win@gmail.com>
2022-06-13 19:31:14 +0200

    removed unused table

--- models/users.sql
+++
@@ -1,8 +0,0 @@
-USE [Terminator]
-
-CREATE TABLE [dbo].[users] (
-    id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	username varchar(255) NOT NULL UNIQUE,
-	password varchar(255) NOT NULL, 
-	lastlogin bigint NOT NULL
-);
