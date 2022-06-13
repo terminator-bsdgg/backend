8900ad0c5ee72cdb3d4fffe79f1ad7e026c5bccc
Paul Jongmans <pauul.win@gmail.com>
2022-06-13 19:30:51 +0200

    removed userid

--- models/events.sql
+++ models/events.sql
@@ -4,6 +4,5 @@ CREATE TABLE [dbo].[events] (
     id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
     type varchar(255) NOT NULL,
     message text NOT NULL,
-    timestamp bigint NOT NULL,
-    userid int FOREIGN KEY REFERENCES [dbo].[users](id)
+    timestamp bigint NOT NULL
 );
