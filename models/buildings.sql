af62eb220219ac382ebc504e646ff9a18064c2c4
Paul J <56239171+paul-jongmans@users.noreply.github.com>
2022-06-15 12:26:03 +0200

    added documentation for building.sql

--- models/buildings.sql
+++ models/buildings.sql
@@ -1,7 +1,7 @@
-USE [Terminator]
+USE [Terminator] -- Select Terminator database
 
 CREATE TABLE [dbo].[buildings] (
-    id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	name varchar(255) NOT NULL UNIQUE,
-	description VARCHAR(MAX) NOT NULL
-);
+    id int NOT NULL IDENTITY(1,1) PRIMARY KEY, -- id for identification
+	name varchar(255) NOT NULL UNIQUE, -- name of the building
+	description VARCHAR(MAX) NOT NULL -- description of the building
+); -- Creates table dbo.buildings
