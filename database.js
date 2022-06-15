a4057930b5fa2d7692bb51dfc792a401e683994c
Paul J <56239171+paul-jongmans@users.noreply.github.com>
2022-06-15 11:55:20 +0200

    added documentation to database.js

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const sql = require('mssql');
 const sqlConfig = require('./config').sqlConfig;
 
-module.exports.sql = sql;
-module.exports.sqlConfig = sqlConfig;
+module.exports.sql = sql; // Export SQL Module
+module.exports.sqlConfig = sqlConfig; // Export SQL Config
