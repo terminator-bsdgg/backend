93d8ff17ed2d0b9cb6802a3e6ab41a25ffa34fb9
Paul J <56239171+paul-jongmans@users.noreply.github.com>
2022-06-15 12:28:14 +0200

    added documentation to rooms.sql

--- models/rooms.sql
+++ models/rooms.sql
@@ -1,8 +1,8 @@
-USE [Terminator]
+USE [Terminator] -- Select Terminator database
 
 CREATE TABLE [dbo].[rooms] (
-    id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	name varchar(255) NOT NULL UNIQUE,
-	description VARCHAR(MAX) NOT NULL, 
-    buildingid int FOREIGN KEY REFERENCES [dbo].[buildings](id)
-);
+    id int NOT NULL IDENTITY(1,1) PRIMARY KEY, -- id for identification
+	name varchar(255) NOT NULL UNIQUE, -- name of room
+	description VARCHAR(MAX) NOT NULL,  -- description of room
+    buildingid int FOREIGN KEY REFERENCES [dbo].[buildings](id) -- id of building where room is
+); -- Create table dbo.rooms
