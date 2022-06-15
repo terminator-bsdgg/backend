8af51adca788c89a8890fc1ba40b51aae7f4606f
Paul J <56239171+paul-jongmans@users.noreply.github.com>
2022-06-15 11:49:04 +0200

    added documentation

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const config = require('./config');
 const eventUtils = require('./lib/eventUtils');
 
 const gracefulShutdown = (msg) => {
-    eventUtils.addEvent('error', 'Backend stopped with message: ' + msg);
+    eventUtils.addEvent('error', 'Backend stopped with message: ' + msg); // Add event to database
 
     ora().info('Shutdown initiated: ' + msg);
     ora().info('Shutting down...');
@@ -20,16 +20,18 @@ app.listen(config.port, () => {
     process.on('unhandledRejection', gracefulShutdown); // Prevent dirty exit on unhandled promise rejection
 
     database.sql.connect(config.sqlConfig, (error) => {
+        // Connect to database with connection info given in config.json
         if (error) {
-            ora().fail('Database connection error');
+            ora().fail('Database connection error'); // Display error message
             return gracefulShutdown('Database connection error');
         }
 
+        // Print success message
         ora().succeed('Database connected');
         ora().succeed('Backend started on port ' + config.port);
-        eventUtils.addEvent('success', 'Backend started on port ' + config.port);
+        eventUtils.addEvent('success', 'Backend started on port ' + config.port); // Add event to database
     });
 }).on('error', (error) => {
-    ora().fail('Backend failed to start (' + error.message + ')');
-    process.exit();
+    ora().fail('Backend failed to start (' + error.message + ')'); // Display error message
+    process.exit(); // Close the program
 });
